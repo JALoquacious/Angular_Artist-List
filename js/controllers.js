09b920882cdfc9b10cf5d5faa902cf8e2edf31dc
JALoquacious <jalikoudis@protonmail.com>
2017-06-13 22:15:19 -0400

    Add filtering functionality; begin app segmentation

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,8 @@
-let app = angular.module('main', []);
+let artistControllers = angular.module('main', []);
 
-app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+artistControllers.controller('ListController', ['$scope', '$http', function ($scope, $http) {
     $http.get('js/data.json').success(function(data) {
         $scope.artists = data;
+        $scope.artistOrder = 'name';
     });
 }]);
